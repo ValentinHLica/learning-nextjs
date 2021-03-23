3650d7fa8b301ec5f2e1a47d66c22b757b3f02ba
Valentin Lica <licavalentinh@gmail.com>
2021-03-23 22:01:02 +0100

    Added styleing and new page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const HomePage: React.FC = () => {
     const { t } = useTranslation("homepage");
+    const router = useRouter();
+    const [lang, setLang] = useState(router.locale);
 
-    return <div>{t("title")}</div>;
+    const onClick = () => {
+        setLang(lang === "en" ? "al" : "en");
+        router.push("/", `/${lang}`, { locale: lang });
+    };
+
+    return (
+        <div>
+            <h1>{t("title")}</h1>
+
+            <Link href="/about" locale={lang}>
+                About Page
+            </Link>
+
+            <button onClick={onClick}>Change Language to: {lang}</button>
+        </div>
+    );
 };
 
 export const getStaticProps = async ({ locale }) => ({
